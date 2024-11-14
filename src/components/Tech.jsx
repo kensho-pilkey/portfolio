6e10a06764ca43a8425d34c681b55dc74c90e2d5
kensho-pilkey <kpilkey@unc.edu>
2024-11-13 20:09:03 -0500

    Add technology title

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -3,6 +3,7 @@
 import React from "react";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
+import { styles } from "../styles";
 
 const Tech = () => {
   // Group technologies by category
@@ -23,41 +24,46 @@ const Tech = () => {
   ];
 
   return (
-    <div className="flex flex-col gap-10">
-      {orderedCategories.map((row, rowIndex) => (
-        <div key={rowIndex} className="flex flex-col md:flex-row gap-10">
-          {row.map((categoryName) => (
-            <div
-              key={categoryName}
-              className="flex-1 bg-black-100 p-8 rounded-2xl"
-            >
-              <h3 className="text-white text-2xl font-bold mb-6">
-                {categoryName}
-              </h3>
-              <div className="grid grid-cols-5 gap-8">
-                {categories[categoryName] &&
-                  categories[categoryName].map((technology) => (
-                    <div
-                      className="flex flex-col items-center"
-                      key={technology.name}
-                    >
-                      <img
-                        src={technology.icon}
-                        alt={technology.name}
-                        className="w-20 h-20 object-contain"
-                      />
-                      <p className="text-white mt-2 text-center">
-                        {technology.name}
-                      </p>
-                    </div>
-                  ))}
+    <>
+      <span className={styles.sectionSubText}>Proficient</span>
+      <h3 className={styles.sectionHeadText}>Technologies</h3>
+      <div className="flex flex-col gap-10 mt-12">
+        {orderedCategories.map((row, rowIndex) => (
+          <div key={rowIndex} className="flex flex-col md:flex-row gap-10">
+            {row.map((categoryName) => (
+              <div
+                key={categoryName}
+                className="flex-1 bg-black-100 p-8 rounded-2xl"
+              >
+                <h3 className="text-white text-2xl font-bold mb-6">
+                  {categoryName}
+                </h3>
+                <div className="grid grid-cols-5 gap-8">
+                  {categories[categoryName] &&
+                    categories[categoryName].map((technology) => (
+                      <div
+                        className="flex flex-col items-center"
+                        key={technology.name}
+                      >
+                        <img
+                          src={technology.icon}
+                          alt={technology.name}
+                          className="w-20 h-20 object-contain"
+                        />
+                        <p className="text-white mt-2 text-center">
+                          {technology.name}
+                        </p>
+                      </div>
+                    ))}
+                </div>
               </div>
-            </div>
-          ))}
-        </div>
-      ))}
-    </div>
+            ))}
+          </div>
+        ))}
+      </div>
+    </>
   );
+  
 };
 
 export default SectionWrapper(Tech, "");
